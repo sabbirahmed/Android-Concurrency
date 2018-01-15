264b7bd7f5ebe17d0bbbe714ec257add2a153222
Sabbir <sa.webhawksit@gmail.com>
2018-01-15 16:15:56 +0600

    first commit

---
+++ app/src/main/java/me/sabbirahmed/androidconcurrency/MainActivity.java
@@ -0,0 +1,59 @@
+package me.sabbirahmed.androidconcurrency;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ScrollView mScroll;
+    private TextView mTextView;
+    private ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mScroll = findViewById(R.id.scrollViewLog);
+        mTextView = findViewById(R.id.textViewLog);
+        mProgressBar = findViewById(R.id.progressBar);
+
+    }
+
+    public void runCode(View view) {
+        log("Run code");
+
+    }
+
+    private void log(String message) {
+        mTextView.append(message + "\n");
+        scrollTextToEnd();
+    }
+
+    public void clearOutput(View view) {
+        mTextView.setText("");
+        scrollTextToEnd();
+    }
+
+    private void scrollTextToEnd() {
+        mScroll.post(new Runnable() {
+            @Override
+            public void run() {
+                mScroll.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
+    }
+
+    private void displayProgressBar(boolean display){
+        if (display){
+            mProgressBar.setVisibility(View.VISIBLE);
+        } else {
+          mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+}
