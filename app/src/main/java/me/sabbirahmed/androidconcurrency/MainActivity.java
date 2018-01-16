b5af49264b6275dda875a43b5ec33d3134c352c7
Sabbir <sa.webhawksit@gmail.com>
2018-01-16 15:11:43 +0600

    Define and start a background thread

--- app/src/main/java/me/sabbirahmed/androidconcurrency/MainActivity.java
+++ app/src/main/java/me/sabbirahmed/androidconcurrency/MainActivity.java
@@ -3,6 +3,7 @@ package me.sabbirahmed.androidconcurrency;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private ScrollView mScroll;
     private TextView mTextView;
     private ProgressBar mProgressBar;
@@ -28,15 +30,26 @@ public class MainActivity extends AppCompatActivity {
     public void runCode(View view) {
         log("Run code");
         displayProgressBar(true);
+
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                displayProgressBar(false);
+                Log.e(TAG, "Run: starting thread after 4 seconds");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Log.e(TAG, "Run: end thread");
             }
         };
 
-        Handler handler = new Handler();
-        handler.postDelayed(runnable, 3000);
+//        Handler handler = new Handler();
+//        handler.postDelayed(runnable, 3000);
+
+        Thread thread = new Thread(runnable);
+        thread.start();
 
     }
 
