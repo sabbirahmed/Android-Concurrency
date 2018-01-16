26f5516c1cbae2acaf963841d4c8bd2abd04bb77
Sabbir <sa.webhawksit@gmail.com>
2018-01-16 15:46:23 +0600

    Send messages to the UI from threads

--- app/src/main/java/me/sabbirahmed/androidconcurrency/MainActivity.java
+++ app/src/main/java/me/sabbirahmed/androidconcurrency/MainActivity.java
@@ -1,6 +1,8 @@
 package me.sabbirahmed.androidconcurrency;
 
 import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,9 +14,12 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final String MESSAGE_KEY = "message_key";
     private ScrollView mScroll;
     private TextView mTextView;
     private ProgressBar mProgressBar;
+
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
         mScroll = findViewById(R.id.scrollViewLog);
         mTextView = findViewById(R.id.textViewLog);
         mProgressBar = findViewById(R.id.progressBar);
+
+        mHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                Bundle bundle = msg.getData();
+                String message = bundle.getString(MESSAGE_KEY);
+                log(message);
+                displayProgressBar(false);
+            }
+        };
 
     }
 
@@ -40,8 +55,11 @@ public class MainActivity extends AppCompatActivity {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
-                Log.e(TAG, "Run: end thread");
+                Message message = new Message();
+                Bundle bundle = new Bundle();
+                bundle.putString(MESSAGE_KEY, "Thread is complete");
+                message.setData(bundle);
+                mHandler.sendMessage(message);
             }
         };
 
