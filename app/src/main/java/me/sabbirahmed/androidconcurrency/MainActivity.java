01b44c590ee94a32dad1261bdc6c95e3a965667e
Sabbir <sa.webhawksit@gmail.com>
2018-01-22 12:37:20 +0600

    Define and run an IntentService class

--- app/src/main/java/me/sabbirahmed/androidconcurrency/MainActivity.java
+++ app/src/main/java/me/sabbirahmed/androidconcurrency/MainActivity.java
@@ -1,19 +1,13 @@
 package me.sabbirahmed.androidconcurrency;
 
-import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import me.sabbirahmed.androidconcurrency.services.MyIntentService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,8 +15,6 @@ public class MainActivity extends AppCompatActivity {
     private ScrollView mScroll;
     private TextView mTextView;
     private ProgressBar mProgressBar;
-    private MyTask mTask;
-    private boolean mTaskRun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,16 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void runCode(View view) {
 
-        if (mTaskRun && mTask != null){
-            mTask.cancel(true);
-            mTaskRun = false;
-        } else {
-            mTask = new MyTask();
-            mTask.execute("sabbir", "Rumana", "shuvo", "ashik");
-            mTaskRun = true;
-        }
+        log("Running Code");
 
-
+        MyIntentService.startActionFoo(this, "Value 1", "Value 2");
     }
 
     private void log(String message) {
@@ -74,47 +59,6 @@ public class MainActivity extends AppCompatActivity {
             mProgressBar.setVisibility(View.VISIBLE);
         } else {
           mProgressBar.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    class MyTask extends AsyncTask<String, String, String> {
-
-        @Override
-        protected String doInBackground(String... strings) {
-            for (String value: strings){
-                if (isCancelled()){
-                    publishProgress("Cancelled");
-                    break;
-                }
-                Log.i("String Value", value);
-                publishProgress(value);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            return "Thread all is done";
-        }
-
-        @Override
-        protected void onProgressUpdate(String... values) {
-            log(values[0]);
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            log(s);
-        }
-
-        @Override
-        protected void onCancelled() {
-            log("Task cancel");
-        }
-
-        @Override
-        protected void onCancelled(String s) {
-            log("cancelled" + s);
         }
     }
 
